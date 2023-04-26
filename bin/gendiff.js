27f96450eb9f6a336ec7d0c365ea51833cddedfb
LevKrestuaninov <krestuaninovlev@gmail.com>
2023-04-26 08:29:24 +0300

    make lint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,25 +8,23 @@ program
   .name('gendiff ')
   .description(' Compares two configuration files and shows a difference.')
   .version('0.8.0')
-  
+
   .argument('<filepath1>', 'path to file 1')
   .argument('<filepath2>', 'path to file 1')
   .option('-f, --format <type>', 'output format', 'stylish')
   .parse(process.argv);
 
-  const options = program.opts();
-  const [arg1, arg2] = program.args;
-  
-  console.log(genDiff(arg1, arg2, options.format));
-  
-  export default genDiff;
+const options = program.opts();
+const [arg1, arg2] = program.args;
 
+console.log(genDiff(arg1, arg2, options.format));
 
-  /*
+export default genDiff;
+
+/*
   .action((filepath1, filepath2) => {
     const diff = genDiff(filepath1, filepath2, program.opts());
     console.log(diff);
   });
-
 
 */
