e724b5e8d65c78392a2270f28d354a6e55990748
LevKrestuaninov <krestuaninovlev@gmail.com>
2023-04-26 07:59:00 +0300

    add index.js to ./

--- src/idex.js
+++ src/idex.js
@@ -1,4 +1,3 @@
-// import _ from 'lodash';
 import getFile from './getFile.js';
 import genDiff from './genDiff.js';
 import getFormat from './formatters/index.js';
@@ -9,7 +8,5 @@ export default (filepath1, filepath2, formatName) => {
 
   const format = getFormat(formatName);
   const result = format(genDiff(file1, file2));
-
-  // const result = makePlain(genDiff(file1, file2))
   return result;
 };
